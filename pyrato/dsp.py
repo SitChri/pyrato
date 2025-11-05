af0e7cb09c8bfc8cd81e55c311c9ac370e5dfdbd
SitChri <sittlchris@web.de>
2025-11-05 10:31:12 +0100

    greetings from monty python flying circus

--- pyrato/dsp.py
+++ pyrato/dsp.py
@@ -149,7 +149,7 @@ def find_impulse_response_maximum(
             "The SNR seems lower than the specified threshold value. Check "
             "if this is a valid impulse response with sufficient SNR.",
             stacklevel=2)
-
+    print("The ministiry of silly walks suggests another walk!")
     return max_sample
 
 
